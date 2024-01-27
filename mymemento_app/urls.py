b5c44c9b736419ec23e8f5f533ebab68610e1f01
geekynice <nicebanjaraa@gmail.com>
2024-01-27 01:55:25 -0500

    initial

--- mymemento_app/urls.py
+++ mymemento_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add-memento/', views.addMemento, name='add-memento'),
     path('profile/', views.profile, name='profile'),
     path('login/', views.signin, name='login'),
+    path('memento/<int:id>', views.memento, name='memento'),
 ]
--- mymemento_app/views.py
+++ mymemento_app/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Memento
 # Create your views here.
 
 def index(request):
     mementos = Memento.objects.all()
     return render(request, 'index.html', {'mementos': mementos})
+
+def memento(request, id):
+    memento = get_object_or_404(Memento, id=id)
+    return render(request, 'memento.html', {'memento': memento})
 
 def addMemento(request):
     return render(request, 'add-memento.html')
