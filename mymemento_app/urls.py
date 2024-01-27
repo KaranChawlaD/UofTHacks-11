c9e9d3dc9e80727ecfa7698b592be6818212bd43
geekynice <nicebanjaraa@gmail.com>
2024-01-27 02:24:20 -0500

    production

--- mymemento_app/urls.py
+++ mymemento_app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path,include
 from mymemento_app import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name="index"),
@@ -7,4 +11,7 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('login/', views.signin, name='login'),
     path('memento/<int:id>', views.memento, name='memento'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
